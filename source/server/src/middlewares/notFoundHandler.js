c56c77712805f2b23f77cc2b47e75edeaefef7e1
minh_nghia <minhnghia2003s@gmail.com>
2026-05-10 12:31:42 +0700

    remove source from main

--- source/server/src/middlewares/notFoundHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Middleware xử lý các route không tồn tại.
- */
-export function notFoundHandler(req, res) {
-  return res.status(404).json({
-    success: false,
-    message: `Không tìm thấy route ${req.originalUrl}`,
-  });
-}
